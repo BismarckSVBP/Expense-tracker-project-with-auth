0a47b817f7b1ed065203a348f98c68f0bb64257a
BismarckSVBP <163289874+BismarckSVBP@users.noreply.github.com>
2024-06-12 22:23:34 +0530

    Update sleeptracker.js

--- routes/sleeptracker.js
+++ routes/sleeptracker.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
 
-  return res.end("Chale ja Bossdk project abhi complete nahi hua hai hone ke baad aana...");
-  // return res.end("Project is Under-Deplopment.So you can enjoy other features of my web-project.Good Bye Baby...");
+  
+   return res.end("Project is Under-Deplopment.So you can enjoy other features of my web-project.Good Bye Baby...");
 });
 
 module.exports = router;
